2514ff0a5e7ca03a9bd833fbd1d683f105965243
luiz-david05 <decd393@gmail.com>
2022-04-08 14:21:19 -0300

    Update sum.js

--- sum.js
+++ sum.js
@@ -1,14 +1,12 @@
 import { print, input } from './utils.js'
-
 function main(){
-    //inicio
+    //start
     print('somar de 1 até 200')
     const start = 1
     const end = Number(input('valor: '))
-
-    //processo
+    //process
     const sum = (start+end) * (end/2)
-
+    //output
     print(`a soma é: ${sum}`)
 }
 main()
