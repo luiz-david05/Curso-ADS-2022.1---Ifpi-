53133a249353b89908ac61a2a4940f8d1185e080
luiz-david05 <decd393@gmail.com>
2022-05-20 14:24:01 -0300

    update input_utils.js

--- wordplay/input_utils.js
+++ wordplay/input_utils.js
@@ -4,30 +4,6 @@ export function enter_to_continue(){
     input('<enter> para continuar...')
     console.clear()
 }
-export function pegar_numero_positivo(msg){
-    return pegar_numero_no_intervalo(msg, 0, Number.valor_max)
-}
-
-export function pegar_numero_no_intervalo(msg, min, max){
-    let numero = pegar_numero(msg)
-    
-    while (!(numero >= min && numero <= max)){
-        console.log(`O número (${numero}) está fora da faixa [${min}-${max}]`)
-        numero = pegar_numero(msg)
-    }
-
-    return numero
-}
-
-export function pegar_numero(msg){
-    let valor = Number(input(msg))
-    if (isNaN(valor)){
-        console.log('Digite um valor numérico')
-        valor = pegar_numero(msg)
-    }
-    return valor
-}
-
 export function pegar_texto(msg){
     const valor = input(msg)
     if (valor.length === 0){
